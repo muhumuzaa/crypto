8f72b3d3fd61e54c3fe9824410d77fef79f44238
Ambroze Muhumuza <142443950+muhumuzaa@users.noreply.github.com>
2023-10-16 23:00:21 +0000

    css2

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
 import "./navbar.css"
+import logo from "../../assets/logo.svg"
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='gpt3__navbar'>
+      <div className='gpt3__navbar-links'>
+        <div className='gpt3__navbar-links_logo'>
+          <img src={logo} alt='logo' /> 
+        </div>
+        <div className='gpt3__navbar-links_container'>
+        <a href='#home'>Home</a>
+        <a href='#wgtp3'>What is GPT3</a>
+        <a href='#posibility'>Open AI</a>
+        <a href='#features'>case Studies</a>
+        <a href='#blog'>Library</a>
+        </div>
+        <div className='gpt3__navbar-sign'>
+        <p>Sign In</p>
+        <button type='button'>Sign Up</button>
+      </div>
+      </div>
+     
+    </div>
   )
 }
 
