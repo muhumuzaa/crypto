21c72c6f80235a6d3088c3493c9652600185176b
Ambroze Muhumuza <142443950+muhumuzaa@users.noreply.github.com>
2023-10-17 03:14:57 +0000

    customer slide

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,6 +1,17 @@
 import React, { useState } from 'react'
 import "./navbar.css"
 import logo from "../../assets/logo.svg"
+import { RiCloseLine, RiMenuLine } from "react-icons/ri";
+
+const Menu =()=>{
+  <> 
+  <a href='#home'>Home</a>
+  <a href='#wgtp3'>What is GPT3</a>
+  <a href='#posibility'>Open AI</a>
+  <a href='#features'>case Studies</a>
+  <a href='#blog'>Library</a>
+  </>
+}
 
 const Navbar = () => {
 
@@ -13,18 +24,34 @@ const Navbar = () => {
         </div>
         <div className='gpt3__navbar-links_container'>
         <a href='#home'>Home</a>
-        <a href='#wgtp3'>What is GPT3</a>
-        <a href='#posibility'>Open AI</a>
-        <a href='#features'>case Studies</a>
-        <a href='#blog'>Library</a>
+  <a href='#wgtp3'>What is GPT3</a>
+  <a href='#posibility'>Open AI</a>
+  <a href='#features'>case Studies</a>
+  <a href='#blog'>Library</a>
         </div>
         <div className='gpt3__navbar-sign'>
         <p>Sign In</p>
         <button type='button'>Sign Up</button>
       </div>
       <div className='gpt3__navbar-menu'>
-{toggleMenu? <RiCloseLine color ="#fff" size ={27} onClick={()=> setToggleMenu(false) } />:
-<RiMenuLine color ="#fff" size ={27} onClick={()=> setToggleMenu(true) } />}
+{toggleMenu ? <RiCloseLine color ="#fff" size ={27} onClick={()=> setToggleMenu(false) } />
+          :<RiMenuLine color ="#fff" size ={27} onClick={()=> setToggleMenu(true) } />}
+
+{toggleMenu && (<div className='gpt3__navbar-menu_container scale-up-center'>
+  <div className='gpgpt3__navbar-menu_container-links'>
+  {/* <Menu /> */}
+  <a href='#home'>Home</a>
+  <a href='#wgtp3'>What is GPT3</a>
+  <a href='#posibility'>Open AI</a>
+  <a href='#features'>case Studies</a>
+  <a href='#blog'>Library</a>
+
+  <div className='gpt3__navbar-menu_container-links-sign'>
+        <p>Sign In</p>
+        <button type='button'>Sign Up</button>
+      </div>
+  </div>
+</div>)}
 
       </div>
       </div>
--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
-import "./header.css"
+import React from 'react';
+import "./header.css";
+import people from "../../assets/people.png";
+import ai from "../../assets/ai.png";
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='gpt3__header section__padding'>
+      <div className='gpt3__header-content'>
+        <h1 className='gradient__text'>
+        It is a long established fact that a reader will be distracted by the readable content of a page 
+        </h1>
+        <p>
+        It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum
+        </p>
+        <div className='gpt3__header-content__input'>
+          <input type='email' placeholder='Your email address'/>
+          <button type='button'>Get Started</button>
+       
+        </div>
+        <div className='gpt3__header-content__people'>
+          <img src={people} alt='people'/>
+          <p>mmy text ever since the 1500s, when an</p>
+        </div>
+      
+       
+      </div>
+      <div className='gpt3__header-image'>
+          <img src={ai} alt='ai'/>
+        </div>
+    </div>
   )
 }
 
