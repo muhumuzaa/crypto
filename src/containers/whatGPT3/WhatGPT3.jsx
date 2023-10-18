c0ee82b99e99ef78b741dc06585748163d71312d
Ambroze Muhumuza <142443950+muhumuzaa@users.noreply.github.com>
2023-10-18 02:32:38 +0000

    below haeder section

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
 import "./whatGPT3.css"
+import { Feature } from "../../components/Feature"
 
 const WhatGPT3 = () => {
   return (
-    <div>WhatGPT3</div>
+    <div className="gpt3__whatgpt3 section__margin" id='whgpt3'>
+      <div className='gpt3__whatgpt3-feature'>
+      <Feature title ="What is GPT-3" text= "book or other written or printed work, regarded in terms of its content rather than its physicalbook or other written or printed work, regarded in terms of its content rather than its physicalbook or other written or printed work, regarded in terms of its content rather than its physical"/>
+      </div>
+<div className='gpt3__whatgpt3-heading'>
+  <h1 className='gradient__text'></h1>
+  <p>Explore the library</p>
+</div>
+<div className='gpt3__whatgpt3-container'>
+<Feature title ="ChatBot" text= "book or other written or printed work, regarded in terms of its content rather than its physicalbook or other written or printed work, regarded in terms of its content rather than its physicalbook or other written or printed work, regarded in terms of its content rather than its physical"/>
+<Feature title ="Knowledge Base" text= "book or other written or printed work, regarded in terms of its content rather than its physicalbook or other written or printed work, regarded in terms of its content rather than its physicalbook or other written or printed work, regarded in terms of its content rather than its physical"/>
+<Feature title ="Education" text= "book or other written or printed work, regarded in terms of its content rather than its physicalbook or other written or printed work, regarded in terms of its content rather than its physicalbook or other written or printed work, regarded in terms of its content rather than its physical"/>
+</div>
+    </div>
   )
 }
 
